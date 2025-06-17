8f81a66d3028bd76ef2ec975cabe70e58cb770c0
Mike124-r <tontnoel@outlook.com>
2025-06-17 20:07:08 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,9 @@
+author = 'How-to-Log-IN-to-Dashlane-A-Step-by-Step-Guide'
+release = '1.0'
+
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
